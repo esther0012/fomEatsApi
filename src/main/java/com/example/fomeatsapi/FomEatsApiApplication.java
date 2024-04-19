c12348239aa70de0737a4cd795cea7617df66c88
esther <esthermassabane08@gmail.com>
2024-04-18 23:48:18 -0300

    banco h2

---
+++ src/main/java/com/example/fomeatsapi/FomEatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fomeatsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FomEatsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FomEatsApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/fomeatsapi/controller/login.java
@@ -0,0 +1,4 @@
+package com.example.fomeatsapi.controller;
+
+public class login {
+}
